890fb1751352b1dd05252939d39fc9a9c393373d
Gig2341 <danmata2341@gmail.com>
2024-01-12 15:50:00 +0000

    task 1 to 4

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+"""this module represents the authentication class
+"""
+
+
+from flask import Flask, request
+from typing import List, TypeVar
+
+class Auth:
+    """this represents the authentication
+    """
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """defines the require_auth method
+        """
+        if path is None or excluded_paths is None or not excluded_paths:
+            return True
+        if path in excluded_paths:
+            return False
+        # Ensure that all paths in excluded_paths end with a slash
+        excluded_paths = [p if p.endswith('/') else p + '/' for p in excluded_paths]
+        # Check if the path is in excluded_paths
+        return path + '/' not in excluded_paths
+    def authorization_header(self, request=None) -> str:
+        """
+        """
+        return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """
+        """
+        return None
