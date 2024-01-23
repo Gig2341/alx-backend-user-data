8b71742e0a369c59cbe7359afbfc794934c9bab2
Gig2341 <danmata2341@gmail.com>
2024-01-23 12:08:49 +0000

    hashed password implimented

---
+++ 0x03-user_authentication_service/auth.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+'''the auth module'''
+
+import bcrypt
+
+
+
+def _hash_password(password: str) -> bytes:
+    """Hash the input password using bcrypt with a salt.
+    Args:
+        password (str): The input password to be hashed.
+    Returns:
+        bytes: The salted hash of the input password.
+    """
+    # Generate a random salt and hash the password
+    salt = bcrypt.gensalt()
+    hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
+
+    return hashed_password
