231c82a8413b37b4fee05729ba44a012f4e8003a
Gig2341 <danmata2341@gmail.com>
2024-01-23 10:42:30 +0000

    mode changed

---
+++ 0x03-user_authentication_service/db.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python3
+"""DB module
+"""
+
+
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm.session import Session
+from sqlalchemy.exc import InvalidRequestError
+from sqlalchemy.orm.exc import NoResultFound
+
+from user import Base
+from user import User
+
+class DB:
+        """DB class
+        """
+
+        def __init__(self) -> None:
+            """Initialize a new DB instance
+            """
+            self._engine = create_engine("sqlite:///a.db", echo=True)
+            Base.metadata.drop_all(self._engine)
+            Base.metadata.create_all(self._engine)
+            self.__session = None
+
+        @property
+        def _session(self) -> Session:
+            """Memoized session object
+            """
+            if self.__session is None:
+                DBSession = sessionmaker(bind=self._engine)
+                self.__session = DBSession()
+            return self.__session
+
+        def add_user(self, email: str, hashed_password: str) -> User:
+            '''adds a new user object
+            Args:
+                email (str): User's email
+                hashed_password (str): User's hashed password
+
+            Returns:
+                User: The created User object
+            '''
+            new_user = User(email=email, hashed_password=hashed_password)
+            self._session.add(new_user)
+            self._session.commit()
+            return new_user
+
+        def find_user_by(self, **kwargs: dict) -> User:
+            '''
+            '''
+            try:
+                user = self._session.query(User).filter_by(**kwargs).first()
+                if user is None:
+                    raise NoResultFound("No user found")
+                return user
+            except InvalidRequestError as e:
+                self._session.rollback()
+                raise e
