68c0e429ef5ddcecf0723b6c26ac7feda77704ce
Gig2341 <danmata2341@gmail.com>
2024-01-23 15:43:37 +0000

    simple flask app created

---
+++ 0x03-user_authentication_service/app.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+from flask import Flask, jsonify
+
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def welcome():
+    """GET route to return a JSON payload."""
+    message = {"message": "Bienvenue"}
+    return jsonify(message)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
