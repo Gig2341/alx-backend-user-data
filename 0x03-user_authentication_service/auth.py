fe1e90be5f3d75030ec43d85f9bbb9b8464b6b78
Gig2341 <danmata2341@gmail.com>
2024-01-25 16:50:33 +0000

    password update function done

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -109,3 +109,14 @@ class Auth():
             return user.reset_token
         except NoResultFound:
             raise ValueError
+
+    
+    def update_password(self, reset_token: str, password: str) -> None:
+        """
+        """
+        try:
+            user = self._db.find_user_by(reset_token=reset_token)
+            user.hashed_password = _hash_password(password).decode('utf-8')
+            user.reset_token = None
+        except NoResultFound:
+            raise ValueError
